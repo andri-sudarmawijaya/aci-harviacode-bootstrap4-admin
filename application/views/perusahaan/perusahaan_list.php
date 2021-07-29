3cf47cc321d1c378ce2108c9862c775b5de567bb
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-07-29 11:24:30 +0700

    remove empty space

--- application/views/perusahaan/perusahaan_list.php
+++ application/views/perusahaan/perusahaan_list.php
@@ -1,11 +1,10 @@
 <!-- Container Fluid-->
 <div class="container-fluid" id="container-wrapper">
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Blank Page</h1>
+        <h1 class="h3 mb-0 text-gray-800"><?php echo $title; ?></h1>
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="./">Home</a></li>
-            <li class="breadcrumb-item">Pages</li>
-            <li class="breadcrumb-item active" aria-current="page">Blank Page</li>
+            <li class="breadcrumb-item active" aria-current="page"><?php echo $title; ?></li>
         </ol>        
     </div>
 
